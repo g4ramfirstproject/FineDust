2fe8101de81b0e11288bcd388b2821afe0085833
handnew04 <yamyam1575@gmail.com>
2019-05-08 18:51:57 +0900

    Main Activity View Setup

--- app/src/main/java/com/example/ju/finedust/MainActivity.java
+++ app/src/main/java/com/example/ju/finedust/MainActivity.java
@@ -6,13 +6,21 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     static final String baseURL = "http://openapi.airkorea.or.kr/openapi/services/rest/";
     private PermissionRequest permissionRequest;
     private CurrentLocation mlocation;
+
+    private TextView locationName, currentTime, locationDustLevel, locationDustLevelText, locationFineDustLevel, locationFineDustLevelText;
+    private RecyclerView timeRecyclerView, dailyRecyclerView;
+    private ImageView searchBtn, shareBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        viewSetup();
     }
 
     @Override
@@ -39,6 +49,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else {
             super.onBackPressed();
         }
+    }
+
+    private void viewSetup() {
+        locationName = findViewById(R.id.locationName_tv);
+        currentTime = findViewById(R.id.MainTime_tv);
+        locationDustLevel = findViewById(R.id.MainDustLevel_tv);
+        locationDustLevelText = findViewById(R.id.MainDustLevelText_tv);
+        locationFineDustLevel = findViewById(R.id.MainFineDustLevel_tv);
+        locationFineDustLevelText = findViewById(R.id.MainFineDustLevelText_tv);
+        timeRecyclerView = findViewById(R.id.MainrecyclerView);
+        searchBtn = findViewById(R.id.MainAppBarSearch_iv);
+        shareBtn = findViewById(R.id.MainAppBarShare_iv);
     }
 
 
